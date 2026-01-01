aaea9a39199edaea061f2acca20b79da81292a7a
lorenzosalti <lorenzosalti@gmail.com>
2026-01-01 18:05:07 +0100

    feat: add event for player movement through keyboard arrows

--- src/components/MapGrid.jsx
+++ src/components/MapGrid.jsx
@@ -1,10 +1,38 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import Player from "./Player";
 import PlayerPositionContext from "../contexts/PlayerPositionContext";
 
 function MapGrid({ map }) {
 
-  const { playerPos } = useContext(PlayerPositionContext);
+  const { playerPos, setPlayerPos } = useContext(PlayerPositionContext);
+
+  useEffect(() => {
+
+    const handleKeyDown = (event) => {
+      setPlayerPos((prev => {
+        switch (event.key) {
+          case "ArrowUp":
+            return { x: prev.x, y: prev.y - 1 }
+          case "ArrowDown":
+            return { x: prev.x, y: prev.y + 1 }
+          case "ArrowLeft":
+            return { x: prev.x - 1, y: prev.y }
+          case "ArrowRight":
+            return { x: prev.x + 1, y: prev.y }
+        }
+      }))
+    }
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+
+  }, [setPlayerPos])
+
+
+
 
   return (
     <div className="map-grid">
