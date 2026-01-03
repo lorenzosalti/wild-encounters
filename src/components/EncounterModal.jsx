5bd5c70fb1a531fa4a0d04991d3cba03e0e60f09
lorenzosalti <lorenzosalti@gmail.com>
2026-01-03 14:56:59 +0100

    chore: create encounter modal as component

---
+++ src/components/EncounterModal.jsx
@@ -0,0 +1,28 @@
+
+
+function EncounterModal() {
+
+  return (
+    <>
+      <div className="modal fade show" id="encounterModal" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="encounterModalLabel" aria-hidden="true" style={{ display: "block" }}>
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="encounterModalLabel">ENCOUNTER</h1>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+              ENCOUNTER!!!
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="button" className="btn btn-primary">Understood</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default EncounterModal
