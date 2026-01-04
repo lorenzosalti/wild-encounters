691b4b928c271249a06f525ff7955076c4cab68c
lorenzosalti <lorenzosalti@gmail.com>
2026-01-04 14:43:15 +0100

    feat: add button to close encounter modal

--- src/components/EncounterModal.jsx
+++ src/components/EncounterModal.jsx
@@ -1,12 +1,23 @@
+import { useContext } from "react"
+import GlobalContext from "../contexts/GlobalContext"
 
 
 function EncounterModal() {
+
+  const { setIsEncounter } = useContext(GlobalContext);
+
+  function handleClose() {
+    setIsEncounter(false);
+  }
+
 
   return (
     <>
       <div className="modal" id="encounterModal">
 
         <h1>ENCOUNTER</h1>
+
+        <button type="button" onClick={handleClose}>Close</button>
 
       </div>
     </>
