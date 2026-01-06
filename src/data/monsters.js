e6094a422401c52b8b9b400416fb1a65a68b9f6f
lorenzosalti <lorenzosalti@gmail.com>
2026-01-06 13:42:45 +0100

    chore: create basic monsters list

---
+++ src/data/monsters.js
@@ -0,0 +1,13 @@
+const monstersOne = [
+  { id: 1, name: "Nidoran M", spawnRate: 20, catchRate: 0.5 },
+  { id: 2, name: "Doduo", spawnRate: 20, catchRate: 0.5 },
+  { id: 3, name: "Exeggcute", spawnRate: 20, catchRate: 0.5 },
+  { id: 4, name: "Paras", spawnRate: 15, catchRate: 0.5 },
+  { id: 5, name: "Nidorino", spawnRate: 10, catchRate: 0.5 },
+  { id: 6, name: "Nidoran F", spawnRate: 5, catchRate: 0.5 },
+  { id: 7, name: "Parasect", spawnRate: 5, catchRate: 0.5 },
+  { id: 8, name: "Kangaskhan", spawnRate: 4, catchRate: 0.5 },
+  { id: 9, name: "Scyther", spawnRate: 1, catchRate: 0.5 }
+];
+
+export default monstersOne;
