8826d15f9e7c8ecb4a1f478d5e5e72f8a6f4ffa8
lorenzosalti <lorenzosalti@gmail.com>
2026-01-09 18:33:13 +0100

    feat: create function to pick a rundom monster by spawn rate

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,20 @@ import GlobalContext from "./contexts/GlobalContext.js"
 import { useState, useEffect } from "react"
 
 import mapOne from "./data/maps.js"
+import monstersOne from "./data/monsters.js"
 
 
 function App() {
 
   const [playerPos, setPlayerPos] = useState({ x: 32, y: 10 });
   const [isEncounter, setIsEncounter] = useState(false);
+  const [monster, setMonster] = useState({});
 
   useEffect(() => {
     const { x, y } = playerPos;
 
     if (mapOne[y][x] === "E" && Math.random() < 0.1) {
+      setMonster(getRandomMonster(monstersOne));
       setIsEncounter(true);
     }
   }, [playerPos])
@@ -26,6 +29,8 @@ function App() {
     };
   }, [setPlayerPos, isEncounter])
 
+
+  // UTILITY FUNCTIONS
   function handleKeyDown(event) {
     if (!isEncounter) {
       setPlayerPos((prev => {
@@ -50,12 +55,28 @@ function App() {
     }
   }
 
+  function getRandomMonster(monsters) {
+    const total = monsters.reduce((sum, monster) => sum + monster.spawnRate, 0);
+
+    let random = Math.random() * total;
+
+    console.log(random)
+
+    for (const monster of monsters) {
+      if (random < monster.spawnRate) {
+        console.log(monster);
+        return monster;
+      }
+      random -= monster.spawnRate;
+    };
+  }
+
 
 
 
   return (
     <>
-      <GlobalContext.Provider value={{ playerPos, isEncounter, setIsEncounter }}>
+      <GlobalContext.Provider value={{ playerPos, isEncounter, setIsEncounter, monster }}>
         <Header />
         <MapGrid map={mapOne} />
       </GlobalContext.Provider>
