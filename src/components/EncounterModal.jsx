e7fdfd0157650003b72e4f1f2f3b0501a655e1b2
lorenzosalti <lorenzosalti@gmail.com>
2026-01-13 16:45:56 +0100

    feat: basic catching function

--- src/components/EncounterModal.jsx
+++ src/components/EncounterModal.jsx
@@ -10,6 +10,15 @@ function EncounterModal() {
     setIsEncounter(false);
   }
 
+  function handleCatch(monster) {
+    if (Math.random() < monster.catchRate) {
+      console.log(`Yes! ${monster.name} was caugth!`);
+      setIsEncounter(false);
+    } else {
+      console.log(`Oh no! ${monster.name} got out! Unlucky`);
+    };
+  }
+
 
   return (
     <>
@@ -17,7 +26,8 @@ function EncounterModal() {
 
         <h1>A wild {monster.name} appeared!</h1>
 
-        <button type="button" onClick={handleClose}>Close</button>
+        <button type="button" onClick={() => handleCatch(monster)}>Throw Ball</button>
+        <button type="button" onClick={handleClose}>Run!</button>
 
       </div>
     </>
